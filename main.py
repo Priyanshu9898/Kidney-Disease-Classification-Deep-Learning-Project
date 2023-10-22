448d67143cec577ea21897b86775e41fb4d0c07f
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2023-10-22 11:37:31 +0530

    data ingestion complete

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from src.kidneyDiseaseClassifier import logger
+from src.kidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info('Welcome to')
+STAGE_NAME = "Data Ingestation"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(
+        f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
