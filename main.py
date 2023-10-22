f2b666db175a368977d26aa409c3d9b883e65240
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2023-10-22 18:34:07 +0530

    Model training done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.kidneyDiseaseClassifier import logger
 from src.kidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.kidneyDiseaseClassifier.pipeline.stage_02_model_building import ModelBuildingPipeline
+from src.kidneyDiseaseClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestation"
 
@@ -20,6 +21,18 @@ STAGE_NAME = "Model Building"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = ModelBuildingPipeline()
+    obj.main()
+    logger.info(
+        f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Training"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(
         f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
